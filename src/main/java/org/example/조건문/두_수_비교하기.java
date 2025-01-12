10bec0125a4c89c41ae9a3223b492aa78885b339
dazz6 <dazz6@kakao.com>
2025-01-12 18:50:37 +0900

    1330 두 수 비교하기

---
+++ src/main/java/org/example/조건문/두_수_비교하기.java
@@ -0,0 +1,27 @@
+package org.example.조건문;
+
+import java.util.Scanner;
+
+public class 두_수_비교하기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        String[] result;
+        int result_a, result_b;
+        result = input.split(" ");
+
+        result_a = Integer.parseInt(result[0]);
+        result_b = Integer.parseInt(result[1]);
+
+        String final_result = ">";
+
+        if (result_a == result_b) {
+            final_result = "==";
+        } else if (result_a < result_b) {
+            final_result = "<";
+        }
+
+        System.out.println(final_result);
+    }
+}
