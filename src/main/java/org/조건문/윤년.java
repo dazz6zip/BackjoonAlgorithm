e23aeb4ab28e50a9f297fd231ff3d7797c3f0264
dazz6 <dazz6@kakao.com>
2025-01-12 18:56:36 +0900

    2753 윤년

---
+++ src/main/java/org/조건문/윤년.java
@@ -0,0 +1,17 @@
+package org.조건문;
+
+import java.util.Scanner;
+
+public class 윤년 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int input_year = sc.nextInt();
+        int result = 0;
+        if (input_year % 4 == 0 && input_year % 100 != 0 || input_year % 400 == 0) {
+            result = 1;
+        }
+
+        System.out.println(result);
+    }
+}
