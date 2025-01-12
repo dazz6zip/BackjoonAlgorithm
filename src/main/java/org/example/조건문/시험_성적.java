7357d17a55e6bdeb952779c5c8d19d1dc818cdcd
dazz6 <dazz6@kakao.com>
2025-01-12 18:50:53 +0900

    9498 시험 성적

---
+++ src/main/java/org/example/조건문/시험_성적.java
@@ -0,0 +1,25 @@
+package org.example.조건문;
+
+import java.util.Scanner;
+
+public class 시험_성적 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int score = sc.nextInt();
+
+        String final_score = "F";
+
+        if (score >= 90) {
+            final_score = "A";
+        } else if (score >= 80) {
+            final_score = "B";
+        } else if (score >= 70) {
+            final_score = "C";
+        } else if(score >= 60) {
+            final_score = "D";
+        }
+
+        System.out.println(final_score);
+    }
+}
