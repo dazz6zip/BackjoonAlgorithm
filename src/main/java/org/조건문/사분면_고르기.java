ae0b42ea3e125fcf5fb5b4345c9024d538b40277
dazz6 <dazz6@kakao.com>
2025-01-13 18:37:51 +0900

    14681 사분면 고르기

---
+++ src/main/java/org/조건문/사분면_고르기.java
@@ -0,0 +1,26 @@
+package org.조건문;
+
+import java.util.Scanner;
+
+public class 사분면_고르기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int result = 0;
+
+        if (a > 0 && b > 0) {
+            result = 1;
+        } else if (a < 0 && b < 0) {
+            result = 3;
+        } else if (a > 0 && b < 0) {
+            result = 4;
+        } else if (a < 0 && b > 0) {
+            result = 2;
+        }
+
+        System.out.println(result);
+    }
+}
