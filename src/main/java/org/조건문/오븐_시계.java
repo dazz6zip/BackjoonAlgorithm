6467e70f77a87ca86e92b17c33bd061586c8f59c
dazz6 <dazz6@kakao.com>
2025-01-13 19:02:00 +0900

    2525 오븐 시계

---
+++ src/main/java/org/조건문/오븐_시계.java
@@ -0,0 +1,37 @@
+package org.조건문;
+
+import java.util.Scanner;
+
+public class 오븐_시계 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String now = sc.nextLine();
+        int time = sc.nextInt();
+
+        String[] temp = now.split(" ");
+
+        int hour = Integer.parseInt(temp[0]);
+        int min = Integer.parseInt(temp[1]);
+
+        min += time;
+        if (min >= 60) {
+            int temp_hour = 0;
+            while (true) {
+                min -= 60;
+                temp_hour++;
+                if (min < 60) {
+                    break;
+                }
+            }
+            hour += temp_hour;
+        }
+
+        if(hour > 23) {
+            hour -= 24;
+        }
+
+        System.out.println(hour + " " + min);
+
+    }
+}
