49a05e0f5aaad81a5492ed51692d7b3e86b8264d
dazz6 <dazz6@kakao.com>
2025-01-13 19:06:05 +0900

    2739 구구단

---
+++ src/main/java/org/반복문/구구단.java
@@ -0,0 +1,15 @@
+package org.반복문;
+
+import java.util.Scanner;
+
+public class 구구단 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int input = sc.nextInt();
+
+        for (int i = 1; i < 10; i ++) {
+            System.out.println(input + " * " + i + " = " + input*i);
+        }
+    }
+}
