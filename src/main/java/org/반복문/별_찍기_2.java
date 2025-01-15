e8a17e0c3ed99e830ad819b8101e62181ad3a81c
dazz6 <dazz6@kakao.com>
2025-01-15 22:24:14 +0900

    2439 별 찍기 - 2

---
+++ src/main/java/org/반복문/별_찍기_2.java
@@ -0,0 +1,20 @@
+package org.반복문;
+
+import java.util.Scanner;
+
+public class 별_찍기_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = n - 1; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
