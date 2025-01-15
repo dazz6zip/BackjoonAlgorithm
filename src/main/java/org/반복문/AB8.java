26a2d9dc70fca9347eed76165eecd0acf263d330
dazz6 <dazz6@kakao.com>
2025-01-15 22:10:35 +0900

    11022 A+B - 8

---
+++ src/main/java/org/반복문/AB8.java
@@ -0,0 +1,29 @@
+package org.반복문;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class AB8 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        try {
+            int n = Integer.parseInt(br.readLine());
+
+            for (int i = 0; i < n; i++) {
+                String[] input = br.readLine().split(" ");
+                int a = Integer.parseInt(input[0]);
+                int b = Integer.parseInt(input[1]);
+
+                bw.write("Case #" + (i + 1) +  ": " + a + " + " + b + " = " + (a + b) + "\n");
+            }
+            bw.flush();
+            bw.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
