f63e806a9c8e537f2ef594592c63b92da9e14297
dazz6 <dazz6@kakao.com>
2025-01-15 21:52:48 +0900

    25314 코딩은 체육과목 입니다

---
+++ src/main/java/org/반복문/코딩은_체육과목_입니다.java
@@ -0,0 +1,16 @@
+package org.반복문;
+
+import java.util.Scanner;
+
+public class 코딩은_체육과목_입니다 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int input = sc.nextInt();
+
+        for (int i = 0; i < input / 4 ; i++) {
+            System.out.print("long ");
+        }
+        System.out.println("int");
+    }
+}
