5e78168f865175be442577ccfeedf9d0d1aff1fe
dazz6 <dazz6@kakao.com>
2025-01-16 15:58:47 +0900

    10951 A + B - 4

---
+++ src/main/java/org/반복문/AB4.java
@@ -0,0 +1,16 @@
+package org.반복문;
+
+import java.util.Scanner;
+
+public class AB4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+
+            System.out.println(n + m);
+        }
+    }
+}
