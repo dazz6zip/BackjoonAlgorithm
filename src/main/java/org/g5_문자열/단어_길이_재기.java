4d00fc91ee4f4ef58ed163ff45e81142a0ceb092
dazz6zip <dazz6@kakao.com>
2025-01-20 16:13:58 +0900

    2743 단어 길이 재기

---
+++ src/main/java/org/g5_문자열/단어_길이_재기.java
@@ -0,0 +1,13 @@
+package org.g5_문자열;
+
+import java.util.Scanner;
+
+public class 단어_길이_재기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.nextLine();
+
+        System.out.println(input.length());
+    }
+}
