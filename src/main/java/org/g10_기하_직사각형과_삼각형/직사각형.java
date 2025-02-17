771b6964c2e4c4f56e362cac1be10e1610c927ae
dazz6zip <dazz6@kakao.com>
2025-02-17 13:21:38 +0900

    27323 직사각형

---
+++ src/main/java/org/g10_기하_직사각형과_삼각형/직사각형.java
@@ -0,0 +1,13 @@
+package org.g10_기하_직사각형과_삼각형;
+
+import java.util.Scanner;
+
+public class 직사각형 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A*B);
+    }
+}
