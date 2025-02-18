b31fcb1b3f2fbc601e62a6afb74445ed38497f9e
dazz6zip <dazz6@kakao.com>
2025-02-18 09:56:33 +0900

    15894 수학은 체육과목입니다

---
+++ src/main/java/org/g10_기하_직사각형과_삼각형/수학은체육과목입니다.java
@@ -0,0 +1,10 @@
+package org.g10_기하_직사각형과_삼각형;
+
+import java.util.Scanner;
+
+public class 수학은체육과목입니다 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextLong() * 4);
+    }
+}
