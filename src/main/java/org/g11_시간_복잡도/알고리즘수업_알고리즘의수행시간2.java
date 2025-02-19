9fa1d198dcbd4790c053ae8aaa3af5d8ce8b6245
dazz6zip <dazz6@kakao.com>
2025-02-19 15:19:40 +0900

    24263 알고리즘 수업 - 알고리즘의 수행 시간 2

---
+++ src/main/java/org/g11_시간_복잡도/알고리즘수업_알고리즘의수행시간2.java
@@ -0,0 +1,13 @@
+package org.g11_시간_복잡도;
+
+import java.util.Scanner;
+
+public class 알고리즘수업_알고리즘의수행시간2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println(n);
+        System.out.println(1);
+    }
+}
