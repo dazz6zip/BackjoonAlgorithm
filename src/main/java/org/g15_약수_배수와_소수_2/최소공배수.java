2f2358c9f3344df9b14970be6e4b50de244c577b
dazz6 <dazz6@kakao.com>
2025-03-08 10:46:50 +0900

    1934 최소공배수

--- src/main/java/org/g15_약수_배수와_소수_2/최소공배수.java
+++ src/main/java/org/g15_약수_배수와_소수_2/최소공배수.java
@@ -1,7 +1,34 @@
 package org.g15_약수_배수와_소수_2;
 
-public class 최소공배수 {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.*;
 
+public class 최소공배수 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            int ta = A;
+            int tb = B;
+
+            while (tb != 0) {
+                int t = ta % tb;
+                ta = tb;
+                tb = t;
+            }
+
+            bw.write(((A / ta) * B) + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
